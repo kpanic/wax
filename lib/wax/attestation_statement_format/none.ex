d3318f6e10006f181a1fabf4cf7a588461511068
Marco Milanesi <mmi@starfish.team>
2023-12-06 15:52:55 +0100

    Fix None case

--- lib/wax/attestation_statement_format/none.ex
+++ lib/wax/attestation_statement_format/none.ex
@@ -4,7 +4,8 @@ defmodule Wax.AttestationStatementFormat.None do
   @behaviour Wax.AttestationStatementFormat
 
   @impl Wax.AttestationStatementFormat
-  def verify(_attstmt, _auth_data, _client_data_hash, %Wax.Challenge{attestation: "none"}) do
+  def verify(attstmt, _auth_data, _client_data_hash, _challenge)
+      when map_size(attstmt) == 0 do
     {:ok, {:none, nil, nil}}
   end
 
